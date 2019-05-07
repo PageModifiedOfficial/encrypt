57bbae466ef1fd75b5c8ca709c09060db7611b0d
Mike Benner <refriedchicken@gmail.com>
2019-05-07 11:24:17 -0700

    encryption and decryption functions in place

--- lib/encrypt.ex
+++ lib/encrypt.ex
@@ -1,4 +1,5 @@
 defmodule Encrypt do
+  @aad "AES256GCM"
   @moduledoc """
   Documentation for Encrypt.
   """
@@ -10,5 +11,24 @@ defmodule Encrypt do
   def generate_secret do
     :crypto.strong_rand_bytes(16)
     |> :base64.encode()
+  end
+
+  def encrypt(val, key) do
+    mode = :aes_gcm
+    secret_key = :base64.decode(key)
+    iv = :crypto.strong_rand_bytes(16)
+
+    {ciphertext, ciphertag} =
+      :crypto.block_encrypt(mode, secret_key, iv, {@aad, to_string(val), 16})
+
+    (iv <> ciphertag <> ciphertext) |> :base64.encode()
+  end
+
+  def decrypt(ciphertext, key) do
+    mode = :aes_gcm
+    secret_key = :base64.decode(key)
+    ciphertext = :base64.decode(ciphertext)
+    <<iv::binary-16, tag::binary-16, ciphertext::binary>> = ciphertext
+    :crypto.block_decrypt(mode, secret_key, iv, {@aad, ciphertext, tag})
   end
 end
