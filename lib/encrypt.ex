83620e24a295769f10037f3fc49c7f0eae844b79
Mike Benner <refriedchicken@gmail.com>
2019-05-07 11:09:51 -0700

    Initial project create

---
+++ lib/encrypt.ex
@@ -0,0 +1,18 @@
+defmodule Encrypt do
+  @moduledoc """
+  Documentation for Encrypt.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Encrypt.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
